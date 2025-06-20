7b2cd4a11a5e2dc6d99718434c102291b638e9e7
AdamRB208 <adamrborup@gmail.com>
2025-06-20 12:48:21 -0600

    init

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import { RouterView } from 'vue-router';
+
+</script>
+
+<template>
+  <!-- comment -->
+  <header>
+    <Navbar />
+  </header>
+  <main>
+    <RouterView />
+  </main>
+  <footer class=" text-center">
+    Made with <i class="mdi mdi-heart text-pink"></i> by CodeWorks
+  </footer>
+</template>
+
+<style lang="scss"></style>
---
+++ server/Models/Keep.cs
@@ -0,0 +1,11 @@
+
+public class Keep : RepoItem<int>
+{
+  public string Name { get; set; }
+  public string Description { get; set; }
+  public string Img { get; set; }
+  public int Views { get; set; }
+  public string CreatorId { get; set; }
+  public Account Creator { get; set; }
+  // public VaultKeep Kept { get; set; }
+}
