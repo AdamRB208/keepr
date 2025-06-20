c8f593b8f30c55c0c259c212a849bfd7ebbcf827
AdamRB208 <adamrborup@gmail.com>
2025-06-20 14:04:21 -0600

    created keep controller

---
+++ server/Controllers/KeepController.cs
@@ -0,0 +1,16 @@
+
+[ApiController]
+[Route("api/[controller]")]
+public class KeepController : ControllerBase
+{
+  private readonly KeepService _keepService;
+  private readonly Auth0Provider _auth0Provider;
+
+  public KeepController(KeepService keepService, Auth0Provider auth0Provider)
+  {
+    _keepService = keepService;
+    _auth0Provider = auth0Provider;
+  }
+
+
+}
