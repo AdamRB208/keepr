8bc3d1f5deb6f81acaf9e68eafed9f0d2c634372
AdamRB208 <adamrborup@gmail.com>
2025-06-25 15:53:20 -0600

    updated vault model

--- client/src/models/Vault.js
+++ client/src/models/Vault.js
@@ -3,8 +3,11 @@ export class Vault {
     this.id = data.id
     this.createdAt = new Date(data.createdAt)
     this.updatedAt = new Date(data.updatedAt)
-    this.keepId = data.keepId
-    this.vaultId = data.vaultId
+    this.name = data.name
+    this.description = data.description
+    this.img = data.img
+    this.isPrivate = data.isPrivate
     this.creatorId = data.creatorId
+    this.creator = data.creator
   }
 }
