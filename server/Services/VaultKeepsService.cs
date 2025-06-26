cb23b1d050c948843566f80cdcc294a649bafa65
AdamRB208 <adamrborup@gmail.com>
2025-06-26 10:46:35 -0600

    created vault keeps service

--- server/Services/VaultKeepsService.cs
+++ server/Services/VaultKeepsService.cs
@@ -2,5 +2,11 @@
 public class VaultKeepsService
 {
 
+  private readonly VaultKeepsRepository _vaultKeepsRepository;
+
+  public VaultKeepsService(VaultKeepsRepository vaultKeepsRepository)
+  {
+    _vaultKeepsRepository = vaultKeepsRepository;
+  }
 
 }
