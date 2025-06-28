2fe0a9254d0360db0f71a79b32e78998bd51147c
AdamRB208 <adamrborup@gmail.com>
2025-06-28 00:21:02 -0600

    getting profiles by id and rendering raw profile pojo

--- client/src/pages/ProfilePage.vue
+++ client/src/pages/ProfilePage.vue
@@ -1,13 +1,35 @@
 <script setup>
+import { AppState } from '@/AppState.js';
+import { profilesService } from '@/services/ProfileService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { computed, onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
+const profile = computed(() => AppState.account)
 
+const route = useRoute()
+const router = useRouter()
+
+onMounted(() => {
+  getProfileById()
+})
 // HINT use the route.params
 
 // TODO on mounted get the profile by its profileId
 // TODO on mounted get the keeps by the profileId
 // TODO on mounted get the vaults by the profileId
 
-
+async function getProfileById() {
+  try {
+    await profilesService.getProfileById(route.params.profileId)
+  }
+  catch (error) {
+    Pop.error(error, "COULD NOT GET PROFILE!");
+    logger.error(error)
+    router.push({ name: 'Profile' })
+  }
+}
 
 
 
@@ -18,7 +40,7 @@
 <template>
   <section class="container div row">
     <div class="col-12">
-      Welcome to the Profile Page!
+      <div>{{ profile }}</div>
     </div>
   </section>
 </template>
---
+++ client/src/services/ProfileService.js
@@ -0,0 +1,16 @@
+import { AppState } from "@/AppState.js";
+import { api } from "./AxiosService.js";
+import { logger } from "@/utils/Logger.js";
+import { Account } from "@/models/Account.js";
+
+class ProfileService {
+  async getProfileById(profileId) {
+    AppState.account = null
+    const response = await api.get(`api/profiles/${profileId}`)
+    logger.log('Got Profile!', response.data)
+    AppState.account = new Account(response.data)
+  }
+
+}
+
+export const profilesService = new ProfileService();
